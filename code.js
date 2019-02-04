deb26844eae7e49ff57446a4b8141254e30fcbaf
jepidoptera <jepidoptera@live.com>
2019-02-04 14:22:05 -0600

    upgraded with lizard and Spock options

--- code.js
+++ code.js
@@ -11,37 +11,71 @@ var players = [
 ];
 
 var winnerTable = [
-    [0, 2, 1, 2, 1],
-    [1, 0, 2, 1, 2],
-    [2, 1, 0, 2, 1],
-    [1, 2, 1, 0, 2],
-    [2, 1, 2, 1, 0]
+    [-1, 1, 0, 1, 0],
+    [0, -1, 1, 0, 1],
+    [1, 0, -1, 1, 0],
+    [0, 1, 0, -1, 1],
+    [1, 0, 1, 0, -1]
 ];
+
+var descriptions = [
+    "rock", "smashes", "scissors", "cuts", "paper",
+    "disproves", "Spock", "breaks", "scissors", "beheads", 
+    "lizard", "poisons", "Spock", "vaporizes", "rock", 
+    "crushes", "lizard", "eats", "paper", "covers", "rock"];
 
 var score = [0, 0, 0];
 
 function choose(choice){
-    var player1 = rps[choice];
-    var player2 = response();
-    var winner = chooseWinner(player1, player2);
+    // get human's and computer's moves
+    var weapon = [rps[choice], rps[response()]];
+    // who won?
+    var winner = chooseWinner(weapon[0], weapon[1]);
+    var loser = -(winner - 1);
+    // display info
     document.getElementById("human").textContent = 
-        "human chooses: " + player1;
+        "human chooses: " + weapon[0];
     document.getElementById("computer").textContent = 
-        "computer chooses: " + player2;
-    document.getElementById("winner").textContent = "winner: " + players[winner];
+        "computer chooses: " + weapon[1];
+    var verb;
+    // fancy verbs
+    if (winner >= 0) {
+        var verbPos = descriptions.indexOf(weapon[winner]);
+        verb = descriptions[verbPos + 2] == weapon[loser] ?
+            descriptions[verbPos + 1]
+            : descriptions[descriptions.indexOf(weapon[winner], verbPos + 3) + 1];
+        // describe
+        document.getElementById("verbs").textContent = 
+            weapon[winner] + " " + verb + " " + weapon[loser];      
+    }
+    else {
+        // tie match
+        winner = -1;
+        verb = ["equals", "meets", "matches", "is", "sees"][choice];
+        // describe
+        document.getElementById("verbs").textContent = 
+            weapon[0] + " " + verb + " " + weapon[0];      
+    }
+    // increment score
+    winner ++;
     score[winner]++;
+    // say who won
+    document.getElementById("winner").textContent = "winner: " + players[winner];
+    // display new score
     var scorebox = document.getElementById("score");
-    scorebox.innerHTML = "Score: <br>";
+    scorebox.innerHTML = "";
     for (i = 0; i < score.length; i++){
         scorebox.innerHTML += players[i] + ": " + score[i] + "<br>";
     }
 }
 
 function response(){
-    return rps[parseInt(Math.random() * rps.length)];
+    // just pick on randomly
+    return Math.floor(Math.random() * rps.length);
 }
 
 function chooseWinner(player1, player2){
+    // consult the table
     return winnerTable[rps.indexOf(player1)][rps.indexOf(player2)];
 }
 
@@ -55,11 +89,11 @@ function hideDirections(){
 
 function expand(){
     // let's make this a 5-way game
-    var rps = [
+    rps = [
         "rock",
         "paper",
         "scissors",
-        "spock",
+        "Spock",
         "lizard"
     ];    
     // show the extra buttons
